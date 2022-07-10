62795cd33af5ec59303b7017a1193375de49932e
Hakimrizm <hakimrizm05@gmail.com>
2022-07-10 14:01:38 +0700

    Menambahkan fungsi print, dan mengirit sedikit variable

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector(".btn-submit");
 const container = document.querySelector(".card-form");
+const parent = document.querySelector(".output");
 
 function getValue(el) {
   const value = document.getElementById(el).value;
@@ -8,9 +9,8 @@ function getValue(el) {
 
 function countDate(date1, date2) {
   const oneDay = 24 * 60 * 60 * 1000; // Jam * Menit * Detik * Milisekon
-  const firstDate = date1;
-  const secondDate = date2;
-  return Math.round(Math.abs((firstDate - secondDate) / oneDay));
+  const count = date1 - date2;
+  return Math.round(Math.abs(count / oneDay));
 }
 
 function dateFormat(d) {
@@ -92,7 +92,15 @@ function output(tanggal, nama, jabatan, dpp, gajih, lt, lembur, kasbon, total) {
                 </div>
               </div>
             </div>
-          </div>`;
+          </div>
+          <button type="button" class="btn btn-primary mt-3 cetak">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-printer-fill" viewBox="0 0 16 16">
+              <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5zm6 8H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z" />
+              <path d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
+            </svg>
+            Print
+          </button>
+          `;
 }
 
 btn.addEventListener("click", function () {
@@ -111,7 +119,6 @@ btn.addEventListener("click", function () {
   const kasbon = getValue("kasbon");
 
   const hitung = setIdr(gajih + lt + lembur + kasbon);
-  const parent = document.querySelector(".output");
   const updateUi = output(waktu, nama, jabatan, dpp, gajih, lt, lembur, kasbon, hitung);
 
   container.style.display = "none";
@@ -119,3 +126,9 @@ btn.addEventListener("click", function () {
   parent.innerHTML = updateUi;
 });
 nama.value = "";
+
+parent.addEventListener("click", (e) => {
+  if (e.target.classList.contains("cetak")) {
+    window.print();
+  }
+});
